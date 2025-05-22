6f4a82d293d206d03aa8f58a74ea865874b4110c
raj patel <kakhilesh79@gmail.com>
2025-05-22 23:11:59 +0530

    port added

--- app/database.py
+++ app/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Arpitsuperbase*12@db.rsymeiwcuuuqfdexhzqz.supabase.co:5432/postgres"
+# URL-encoded password: @ → %40
+DATABASE_URL = "mysql+pymysql://u613289423_pprogrammers:Peerprogrammers%403214@srv1910.hstgr.io:3306/u613289423_pprogrammers"
 
-engine = create_engine(DATABASE_URL, echo=True)  # echo=True helps debug SQL
-
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
