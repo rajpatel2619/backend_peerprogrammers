32eb52422994c0c01e22444588b82390648f6a1d
Arpit Anand <thearpit1@gmail.com>
2025-08-08 23:27:34 +0530

    internal server error text remove from exception handling

--- app/routers/users/login.py
+++ app/routers/users/login.py
@@ -62,4 +62,4 @@ def login_user(payload: dict, db: Session = Depends(get_db)):
         }
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
+        raise HTTPException(status_code=500, detail=f"{e}")
