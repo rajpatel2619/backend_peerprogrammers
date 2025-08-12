0f0b557077e086e72559358760b67e694150896f
Arpit Anand <thearpit1@gmail.com>
2025-08-13 01:49:45 +0530

    database with all cp tables

--- app/connection/database.py
+++ app/connection/database.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import sessionmaker
 # DATABASE_URL = "mysql+pymysql://u613289423_pprogrammers:Peerprogrammers%403214@srv1910.hstgr.io:3306/u613289423_pprogrammers"
 
 # # # Development
-DATABASE_URL = "mysql+pymysql://fastapi_dev_user:StrongDevPass%40123@77.37.44.8:3306/fastapi_development"
+# DATABASE_URL = "mysql+pymysql://fastapi_dev_user:StrongDevPass%40123@77.37.44.8:3306/fastapi_development"
 
 
 # # Production
-# DATABASE_U0RL = "mysql+pymysql://fastapi_prod_user:StrongProdPass%40123@77.37.44.8:3306/fastapi_production"
+DATABASE_URL = "mysql+pymysql://fastapi_prod_user:StrongProdPass%40123@77.37.44.8:3306/fastapi_production"
 
 engine = create_engine(DATABASE_URL, echo=True,future=True, pool_pre_ping=True, pool_recycle=280)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
